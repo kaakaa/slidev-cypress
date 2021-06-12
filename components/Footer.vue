5ec31c5e62cb1d3c98970be85367e9adbb5c3fa2
kaakaa <stooner.hoe@gmail.com>
2021-06-12 16:50:35 +0900

    first

---
+++ components/Footer.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { computed, getCurrentInstance } from "vue";
+
+const vm = getCurrentInstance();
+const progress = computed(() => {
+  const nav = vm.appContext.config.globalProperties.$slidev!.nav;
+  return {
+    width: Math.ceil(100 * ((nav.currentPage - 1) / (nav.total - 1))) + "%",
+    height: "5px",
+  };
+});
+</script>
+<template>
+  <div style="width: 100%; height: 5px">
+    <div class="bg-rose-500" :style="progress"></div>
+  </div>
+</template>
